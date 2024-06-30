2b2257039ea28f4c0e8abe57d0f9c5072157c0ff
sahib singh <ss0990100@gmail.com>
2024-06-30 17:29:03 +0530

    adjusted the responses into json object

--- src/service/codeSubmission.js
+++ src/service/codeSubmission.js
@@ -15,25 +15,47 @@ class SubmissionService{
         return minutes * 60 + seconds;
     }
 
-    #splitOutput(output) {
+    #splitRunOutput(output){
+        const executionOutputMatch = output.match(/Execution Output:\n(.*?)\n/);
+        const timingInfoMatch = output.match(/Timing Information:\s*(.*?)\s*\n/);
+        const cpuUsageMatch = output.match(/CPU Usage:\s*(.*?)\s*\n/);
+        const memoryUsageMatch = output.match(/Memory Usage:\s*(.*?)\s*\n/);
+
+        const executionOutput = executionOutputMatch ? executionOutputMatch[1].trim() : null;
+        const timingInfo = timingInfoMatch ? timingInfoMatch[1].trim() : null;
+        const cpuUsage = cpuUsageMatch ? cpuUsageMatch[1].trim() : null;
+        const memoryUsage = memoryUsageMatch ? memoryUsageMatch[1].trim() : null;
+
+        return {
+            executionOutput,
+            timingInfo,
+            cpuUsage,
+            memoryUsage
+        }
+
+    }
+    
+    #splitSubmitOutput(output) {
         try {
-            
-            const parts = output.split(/(?=ALL_TEST_CASES_OUTPUT:|ALL_TEST_CASES_TIMING|ALL_TEST_CASES_MEMORY)/);
-        
+            const parts = output.split(/(?=ALL_TEST_CASES_OUTPUT:|ALL_TEST_CASES_TIMING|ALL_TEST_CASES_MEMORY|ALL_TEST_CASES_CPU)/);
+    
             const stdout = parts[0].split(/(?=ALL_TEST_CASES_OUTPUT:)/)[0].trim();
             const allTestsOutput = parts.find(part => part.startsWith("ALL_TEST_CASES_OUTPUT:")).split("ALL_TEST_CASES_OUTPUT:")[1].trim();
             const allTestsTiming = parts.find(part => part.startsWith("ALL_TEST_CASES_TIMING")).split("ALL_TEST_CASES_TIMING")[1].trim();
             const allTestsMemory = parts.find(part => part.startsWith("ALL_TEST_CASES_MEMORY")).split("ALL_TEST_CASES_MEMORY")[1].trim();
-            
+            const allTestsCpu = parts.find(part => part.startsWith("ALL_TEST_CASES_CPU")).split("ALL_TEST_CASES_CPU")[1].trim();
+    
             const outputArray = allTestsOutput.split("ENDENDEND").map(item => item.trim()).filter(item => item.length > 0);
             const timingArray = allTestsTiming.split("ENDENDEND").map(item => item.trim()).filter(item => item.length > 0).map(this.#convertTimeToSeconds);
             const memoryArray = allTestsMemory.split("ENDENDEND").map(item => parseFloat(item.trim()) / 1024).filter(item => !isNaN(item));
-            
+            const cpuArray = allTestsCpu.split("ENDENDEND").map(item => parseFloat(item.trim().replace('%', ''))).filter(item => !isNaN(item));
+    
             return {
                 stdout,
                 outputArray,
                 timingArray,
-                memoryArray
+                memoryArray,
+                cpuArray
             };
         } catch (error) {
             throw error; 
@@ -43,7 +65,30 @@ class SubmissionService{
 
     async run(sourceCode,stdInput,language,timeLimit,memoryLimit,cpuCoreLimit){
         try {
-            return await codeRunExecution({sourceCode,stdInput,language,timeLimit,memoryLimit,cpuCoreLimit});
+            const UserOutput =  await codeRunExecution({sourceCode,stdInput,language,timeLimit,memoryLimit,cpuCoreLimit});
+            
+            if(UserOutput.slice(0,6)=='stderr'){
+                return UserOutput;
+            }
+
+            const {executionOutput,timingInfo,cpuUsage,memoryUsage} = this.#splitRunOutput(UserOutput);
+
+            console.log({
+                stdInput,
+                stdOutput:executionOutput,
+                RunningTime:timingInfo,
+                CpuUsage:cpuUsage,
+                MemoryUsage:memoryUsage,
+            });
+
+            return {
+                stdInput,
+                stdOutput:executionOutput,
+                RunningTime:timingInfo,
+                CpuUsage:cpuUsage,
+                MemoryUsage:memoryUsage,
+            }
+
         } catch (error) {
             console.log("Error during running of code due to "+error);
             return error;
@@ -69,15 +114,23 @@ class SubmissionService{
                 return UserTestCaseOutput;
             }
 
-            const {stdout,outputArray,timingArray,memoryArray} = this.#splitOutput(UserTestCaseOutput);
+            const {stdout,outputArray,timingArray,memoryArray,cpuArray} = this.#splitSubmitOutput(UserTestCaseOutput);
 
-            const AllTestCaseMatches = (JSON.parse(question.outputResult)).toString() === outputArray.toString();
+            const JudgeOutput = JSON.parse(question.outputResult);
 
-            const AllTestCaseTimingMatches = timingArray.every((testcaseTiming)=> testcaseTiming <= (question.timeLimit));
-
-            const AllTestCaseMemoryMatches = memoryArray.every((testcaseMemory) => testcaseMemory <= (question.memoryLimit));;
-
-            return stdout+`\nOUTPUT_SUCCESS:${AllTestCaseMatches}\nTIME_SUCCESS:${AllTestCaseTimingMatches}\nMEMORY_SUCCESS:${AllTestCaseMemoryMatches}`;
+            const response = [];
+            for(let i=0;i<inputTestcase.length;i++){
+                response.push({
+                    stdInput:inputTestcase[i],
+                    stdOutput:outputArray[i],
+                    judgeOutput:JudgeOutput[i],
+                    RunningTime:`${timingArray[i]} sec`,
+                    CpuUsage:`${cpuArray[i]} %`,
+                    MemoryUsage:`${memoryArray[i]} MB`,
+                    success: ((question.timeLimit>=timingArray[i])&&(question.memoryLimit>=memoryArray[i])&&JudgeOutput[i]===outputArray[i]),
+                });
+            }
+            return response;
 
         } catch (error) {
             console.log("Error during submission of code due to "+error);
